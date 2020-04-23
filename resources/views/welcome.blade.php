659617875b5ca43edac57678720d3bde5a3b04cc
Gaston Montivero <answer@166.62.34.147>
2020-04-23 01:29:51 -0300

    laravel mix - planita coreui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Sumar
                 </div>
 
                 <div class="links">
